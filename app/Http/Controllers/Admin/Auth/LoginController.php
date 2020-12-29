1e794df883ed28d4744957c1c524a5fec98d8a5c
fabriciops <fabriciopps19@gmail.com>
2020-12-28 21:45:13 -0300

    Ajuste de bug

--- app/Http/Controllers/Admin/Auth/LoginController.php
+++ app/Http/Controllers/Admin/Auth/LoginController.php
@@ -4,10 +4,10 @@ namespace App\Http\Controllers\Admin\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
-use Facade\FlareClient\View;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -29,9 +29,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-
     protected $redirectTo = '/painel';
-    // protected $redirectTo = RouteServiceProvider::HOME;
 
     /**
      * Create a new controller instance.
@@ -43,18 +41,45 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
-    public function index(){
-        
+    public function index() {
         return view('admin.login');
     }
 
-    public function authenticate(Type $var = null)
-    {
-        # code...
+    public function authenticate(Request $request) {
+        $data = $request->only([
+            'email',
+            'password'
+        ]);
+        $validator = $this->validator($data);
+
+        $remember = $request->input('remember', false);
+
+        if($validator->fails()) {
+            return redirect()->route('login')
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        if(Auth::attempt($data, $remember)) {
+            return redirect()->route('admin');
+        } else {
+            $validator->errors()->add('password', 'E-mail e/ou senha errados!');
+
+            return redirect()->route('login')
+                ->withErrors($validator)
+                ->withInput();
+        }
     }
 
-    public function logout(){
+    public function logout() {
         Auth::logout();
-        return redirect()->route('painel/login');
+        return redirect()->route('login');
+    }
+
+    protected function validator(array $data) {
+        return Validator::make($data, [
+            'email' => ['required', 'string', 'email', 'max:100'],
+            'password' => ['required', 'string', 'min:4']
+        ]);
     }
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,6 +2,10 @@
 
 @section('title', 'Laravel CMS')
 
+@section('usermenu_enabled')
+<h1>Sair</h1>
+@endsection()
+
 @section('content_header')
     <h1>Painel de controle</h1>
 @endsection()
